aee22b58f81a7e7336f5e8291e19aba629e8203b
garduny <gardunydeveloper@gmail.com>
2026-01-29 20:17:54 +0300

    APP-SKITAGASHY WEB APP

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
 	<title>GASHY BAZAAR | Next Gen Crypto Market</title>
-	<link rel="shortcut icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡</text></svg>">
+	<link rel="shortcut icon" href="public/img/logo.png" type="image/x-icon">
 	<script src="https://cdn.tailwindcss.com"></script>
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
 	<script>
