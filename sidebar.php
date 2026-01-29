1523293553cc117228042c40a300c1e6e66b70b6
garduny <gardunydeveloper@gmail.com>
2026-01-29 20:34:29 +0300

    APP-SKITAGASHY WEB APP

--- sidebar.php
+++ sidebar.php
@@ -56,7 +56,7 @@ function iconClass($target)
                 </span>
                 Home
             </a>
-            <a href="market.php" class="<?= navClass('market') ?>">
+            <a href="market.php" class="<?= navClass('market') ?> <?= navClass('product') ?>">
                 <span class="<?= iconClass('market') ?>">
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z" />
